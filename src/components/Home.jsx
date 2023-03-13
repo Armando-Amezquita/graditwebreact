659729626b1213a9f13efc8a9a9bdaeda5fa4b24
Armando Amezquita <a.amezquita.1009@gmail.com>
2023-03-12 20:53:00 -0500

    delete consolelog and add key in child

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,39 @@ import { MainContainer } from './MainContainer'
 import { Footer } from './Footer'
 
 export function Home() {
-    const [quantity, setQuantity] = useState(0)
-    const [discover, setDiscover] = useState('')
+  const URLPRODUCTSELL = `https://gradistore-spi.herokuapp.com/orders`
+  const URLDISCOVER = `https://gradistore-spi.herokuapp.com/products/all`
+  const [quantity, setQuantity] = useState(0)
+  const [discover, setDiscover] = useState('')
 
-    useEffect( () => async() => {
-        setQuantity( await getDataTotalProductsSell());
-        setDiscover(await getDataTotalCarrousel())
-    }, [])
+  async function getDataTotalProductsSell() {
+    fetch(`${URLPRODUCTSELL}`)
+      .then(res => res.json())
+      .then(res => {
+        const orders = res.orders.nodes;
+        let lineItemsTotal = 0;
 
+        for (const order of orders) {
+          for (const lineitem of order.lineItems.nodes) {
+            lineItemsTotal += lineitem.quantity
+          }
+        }
+        setQuantity(Math.floor(lineItemsTotal / orders.length));
+      })
+  }
 
+  async function getDataTotalCarrousel() {
+    fetch(`${URLDISCOVER}`)
+      .then(data => data.json())
+      .then(res => {
+        setDiscover(res.products.nodes)
+      })
+  }
+
+  useEffect(() => {
+     getDataTotalProductsSell()
+     getDataTotalCarrousel()
+  }, [])
 
   return (
     <div className='container'>
